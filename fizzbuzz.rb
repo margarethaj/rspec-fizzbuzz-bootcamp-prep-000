e1a18e1cf6a946b74c708483ba8ff75e0b46742d
Margaretha Juan <tulip_holy@yahoo.com>
2017-11-01 02:00:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(input)
   input = input.to_i
   if input % 3 == 0 && input % 5 == 0
-     "BuzzFizz"
+     "FizzBuzz"
   elseif input % 3 == 0
     returnStr = "Fizz"
   elseif input % 5 == 0
